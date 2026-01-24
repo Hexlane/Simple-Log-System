85e72fc9008443bbd83451ca5b97d4550b6af93f
Hexlane <deede3771@gmail.com>
2026-01-24 15:34:27 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,8 +5,7 @@ int main()
 {
     base::g_log.attach();
     base::g_log.send("Test", "This is a test log message");
-
-
     std::cout << "Press Enter to exit..." << std::endl;
     std::cin.get();
 }
+
